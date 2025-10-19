f9f7f4239884d3ef6930bd5417eada573b69f736
YenTranSl <tranthikimyen27052004@gmail.com>
2025-10-19 23:59:58 +0700

    fix merge

--- TrainWebAPI/Controllers/AuthController.cs
+++ TrainWebAPI/Controllers/AuthController.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using TrainWeb.Application.Interfaces;
+using TrainWeb.Domain.Entities;
+using TrainWeb.Domain.Enum;
 using TrainWeb.Infrastructure.Services;
 
 namespace TrainWebAPI.Controllers
--- TrainWebAPI/Controllers/GeoController.cs
+++ TrainWebAPI/Controllers/GeoController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using TrainWeb.Infrastructure.Services;
 
 namespace TrainWebAPI.Controllers
 {
